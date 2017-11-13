9b6f09df1543a8c180982807d4afc4e7d65fd5de
Bruno <brunosalustiano303@hotmail.com>
2017-11-13 10:26:53 -0200

    Imagens e primeiras animações

--- core/src/com/cursoandroid/flappybird/FlappyBird.java
+++ core/src/com/cursoandroid/flappybird/FlappyBird.java
@@ -7,27 +7,36 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+	private SpriteBatch batch;//rendeniza as imagens
+	private  Texture passaro;
+	private  Texture fundo;
+
+	//Atributos de configuracao
+	private int movimento = 0;
+	private int larguraDispositivo = 0;
+	private int alturaDispositivo  = 0;
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create () {
+		batch  = new SpriteBatch();
+		passaro = new Texture("passaro1.png");
+		fundo   = new Texture("fundo.png");
+		larguraDispositivo = Gdx.graphics.getWidth();
+		alturaDispositivo  =  Gdx.graphics.getHeight();
 	}
-	
+
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render(){//chamando animacoes
+		movimento ++;
+		batch.begin();//inicia a exibicai das imagnes
+		batch.draw(fundo,0,0,larguraDispositivo,alturaDispositivo);//Colocando e ajeitando o fundo
+		batch.draw(passaro, movimento,400);
+		batch.end();
+
 	}
+
+
+
+
 }
