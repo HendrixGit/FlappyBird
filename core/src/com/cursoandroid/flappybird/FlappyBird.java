ed8781957f41523fe69f84fb30339465a089c5c3
Bruno <brunosalustiano303@hotmail.com>
2017-11-13 18:13:00 -0200

    Canos Parte 1

--- core/src/com/cursoandroid/flappybird/FlappyBird.java
+++ core/src/com/cursoandroid/flappybird/FlappyBird.java
@@ -9,10 +9,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class FlappyBird extends ApplicationAdapter {
 	private SpriteBatch batch;//rendeniza as imagens
 	private  Texture[] passaros;
+	private  Texture canoTopo;
+	private  Texture canoBaixo;
+
 	private  Texture fundo;
 	private float variacao = 0;
 	private float velocidadeQueda = 0;
 	private float posicaoInicialVertical = 0;
+	private float posicaoMovimentoCanoHorizontal = 0;
+	private float espacoEntreCanos = 0;
 
 	//Atributos de configuracao
 	private int larguraDispositivo = 0;
@@ -26,10 +31,16 @@ public class FlappyBird extends ApplicationAdapter {
 		passaros[1] = new Texture("passaro2.png");
 		passaros[2] = new Texture("passaro1.png");
 
+
 		fundo   = new Texture("fundo.png");
+		canoBaixo = new Texture("cano_baixo.png");
+		canoTopo  = new Texture("cano_topo.png");
+
 		larguraDispositivo = Gdx.graphics.getWidth();
 		alturaDispositivo  =  Gdx.graphics.getHeight();
 		posicaoInicialVertical = alturaDispositivo / 2;
+		posicaoMovimentoCanoHorizontal = larguraDispositivo - 100;
+		espacoEntreCanos = 300;
 	}
 
 
@@ -40,17 +51,22 @@ public class FlappyBird extends ApplicationAdapter {
 
 		velocidadeQueda++;
 		if ((posicaoInicialVertical > 0) || (velocidadeQueda < 0)) {
-			posicaoInicialVertical = posicaoInicialVertical - velocidadeQueda;
+				posicaoInicialVertical = posicaoInicialVertical - velocidadeQueda;
 		}
 
 		if (Gdx.input.justTouched()){
+			if (alturaDispositivo - 100 > posicaoInicialVertical) {
 				velocidadeQueda = -15;
 				Gdx.app.log("Toque", "Toque na Tela");
+			}
+
 		}
 
 		batch.begin();//inicia a exibicai das imagnes
 
 		batch.draw(fundo,0,0,larguraDispositivo,alturaDispositivo);//Colocando e ajeitando o fundo
+		batch.draw(canoTopo,posicaoMovimentoCanoHorizontal,alturaDispositivo / 2 + espacoEntreCanos);
+		batch.draw(canoBaixo,posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos /2);
 		batch.draw(passaros[ (int) variacao],30,posicaoInicialVertical);//desenha o passaro
 
 		batch.end();
